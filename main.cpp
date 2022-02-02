cd5f89e4f9b79af9950dc3911c94ebddc04d021a
TopTanatorn <s6230613017@phuket.psu.ac.th>
2022-02-02 15:31:47 +0700

    test

--- main.cpp
+++ main.cpp
@@ -32,20 +32,20 @@ int main()
     //𝛼𝑖−1 𝑎𝑖−1 𝑑𝑖 𝜃i
     for (i = 0; i < num_case; i++)
     {
-        trans_Matrix[i][0][0] = cos(dhP[i][3]);
-        trans_Matrix[i][0][1] = -1 * sin(dhP[i][3]);
+        trans_Matrix[i][0][0] = cos(dhP[i][3]*3.1416/180);
+        trans_Matrix[i][0][1] = -1 * sin(dhP[i][3]*3.1416/180);
         trans_Matrix[i][0][2] = 0;
         trans_Matrix[i][0][3] = dhP[i][1];
 
-        trans_Matrix[i][1][0] = sin(dhP[i][3]) * cos(dhP[i][0]);
-        trans_Matrix[i][1][1] = cos(dhP[i][3]) * cos(dhP[i][0]);
-        trans_Matrix[i][1][2] = -1 * sin(dhP[i][0]);
-        trans_Matrix[i][1][3] = -1 * sin(dhP[i][0]) * dhP[i][2];
+        trans_Matrix[i][1][0] = sin(dhP[i][3]*3.1416/180) * cos(dhP[i][0]*3.1416/180);
+        trans_Matrix[i][1][1] = cos(dhP[i][3]*3.1416/180) * cos(dhP[i][0]*3.1416/180);
+        trans_Matrix[i][1][2] = -1 * sin(dhP[i][0]*3.1416/180);
+        trans_Matrix[i][1][3] = -1 * sin(dhP[i][0]*3.1416/180) * dhP[i][2];
 
-        trans_Matrix[i][2][0] = sin(dhP[i][3]) * sin(dhP[i][0]);
-        trans_Matrix[i][2][1] = cos(dhP[i][3]) * sin(dhP[i][0]);
-        trans_Matrix[i][2][2] = cos(dhP[i][0]);
-        trans_Matrix[i][2][3] = cos(dhP[i][0]) * dhP[i][2];
+        trans_Matrix[i][2][0] = sin(dhP[i][3]*3.1416/180) * sin(dhP[i][0]*3.1416/180);
+        trans_Matrix[i][2][1] = cos(dhP[i][3]*3.1416/180) * sin(dhP[i][0]*3.1416/180);
+        trans_Matrix[i][2][2] = cos(dhP[i][0]*3.1416/180);
+        trans_Matrix[i][2][3] = cos(dhP[i][0]*3.1416/180) * dhP[i][2];
 
         trans_Matrix[i][3][0] = 0;
         trans_Matrix[i][3][1] = 0;
@@ -135,6 +135,7 @@ int main()
         }
         cout << endl;
     }
+    
 
     return 0;
 }
